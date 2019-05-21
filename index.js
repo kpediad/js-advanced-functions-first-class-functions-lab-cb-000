1a3b65357d9817151562cdafc798178d20c78cda
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-21 15:15:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const returnFirstTwoDrivers = function (drivers) {return drivers.slice(0,2);};
 const returnLastTwoDrivers = function (drivers) {return drivers.slice(2);};
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier (multiplier) {return function (fare) {return fare * multiplier;};}
